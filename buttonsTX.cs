acf04fc13e50a618c09ff9bdb141c9f2abb4464e
Riviere <rjs76@njit.edugit config --global user.name Rivieregit config --global user.email rjs76@njit.edu>
2019-06-23 16:51:11 -0400

    Menus and buttons done(35min)

---
+++ buttonsTX.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class buttonsTX : MonoBehaviour
+{
+
+    public void Gamemenu()
+    {
+        SceneManager.LoadScene("gamemenu");
+    }
+    public void Controls()
+    {
+        SceneManager.LoadScene("controls");
+    }
+
+    public void Quitgame()
+    {
+        Application.Quit();
+    }
+
+    public void Playagain()
+    {
+        SceneManager.LoadScene("gamemenu");
+    }
+
+    public void Mainmenu()
+    {
+        SceneManager.LoadScene("playermenu");
+    }
+
+}
