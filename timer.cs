fd0697146880334c9916c0c2c6f957e6fa1f5fb2
Riviere <rjs76@njit.edugit config --global user.name Rivieregit config --global user.email rjs76@njit.edu>
2019-06-23 21:33:31 -0400

    Countdown timer(44min)

---
+++ timer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class timer : MonoBehaviour
+{
+    Image timerbar;
+    public float maxTime = 5f;
+    float timeleft;
+    public GameObject LevelupText;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        LevelupText.SetActive(false);
+        timerbar = GetComponent<Image>();
+        timeleft = maxTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timeleft > 0)
+        {
+            timeleft -= Time.deltaTime;
+            timerbar.fillAmount = timeleft / maxTime;
+        }
+        else
+        {
+            LevelupText.SetActive(true);
+            Time.timeScale = 0;
+        }
+
+    }
+}
